4cd7e4fd955a888b7f2f7eeca5fac592ec0dac35
sam smith <samsmith1@live.com>
2019-04-27 20:12:15 +1000

    location2

--- Location.js
+++ Location.js
@@ -1,11 +1,12 @@
 //get country via ip
+
 function ipLookUp() {
     $.ajax('http://ip-api.com/json')
         .then(
             function success(response) {
                 console.log('User\'s Location Data is ', response);
                 console.log('User\'s Country', response.country);
-                getAdress(response.lat, response.lon)
+                getAddress(response.lat, response.lon)
             },
 
             function fail(data, status) {
@@ -14,15 +15,15 @@ function ipLookUp() {
             }
         );
 }
+
 function getAddress(latitude, longitude) {
-    $.ajax('https://maps.googleapis.com/maps/api/geocode/json?
-          latlng = ' + latitude + ', ' + longitude + ' & key=' + 
-          GOOGLE_MAP_KEY)
+    $.ajax('https://maps.googleapis.com/maps/api/geocode/json?' +
+        'latlng=' + latitude + ',' + longitude + '&key=' +
+        GOOGLE_MAP_KEY)
         .then(
             function success(response) {
                 console.log('User\'s Address Data is ', response)
-                
-                
+                document.getElementById("locationText") = response;
             },
             function fail(status) {
                 console.log('Request failed.  Returned status of',
@@ -30,6 +31,7 @@ function getAddress(latitude, longitude) {
             }
         )
 }
+
 if ("geolocation" in navigator) {
     // check if geolocation is supported/enabled on current browser
     navigator.geolocation.getCurrentPosition(
@@ -42,11 +44,10 @@ if ("geolocation" in navigator) {
         },
         function error(error_message) {
             // for when getting location results in an error
-            console.error('An error has occured while retrieving
-                  location', error_message)
-    ipLookUp()
- }
-});
+            console.error('An error has occured while retrieving' +
+                'location', error_message)
+            ipLookUp()
+        });
 } else {
     // geolocation is not supported
     // get your location some other way
